c7ef9fcb65fbf8cdba10b9d82ab3f470084e0e9c
Antonio Melendez <mel23001@byui.edu>
2024-07-23 23:42:45 -0600

    dark mode

--- scripts/star-wars-generator.js
+++ scripts/star-wars-generator.js
@@ -217,3 +217,19 @@ function displayResults(data) {
 }
 
 apiFetch();
+
+//dark mode button code
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
